9594afdeb01ea9a2bbaff460bdf6c82741bf9869
Nataly Zernova <nz_18@mail.ru>
2024-04-26 20:28:57 +0300

    first commit

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,35 @@
-import {encoded, translations} from './data.js'
+const includeTarget = (value, target = "Id") => value.includes(target);
 
-console.log("Let's rock")
-console.log(encoded, translations)
+export function decoded(data, dictionary) {
+  const uniqueIds = {};
+  const excludeKeys = "groupId service formatSize ca";
 
+  const checkForUnique = (key) => {
+    if (
+      includeTarget(key) &&
+      !(key in uniqueIds) &&
+      !includeTarget(excludeKeys, key)
+    ) {
+      uniqueIds[key] = key;
+    }
+  };
 
+  function convert(obj) {
+    for (let key in obj) {
+      checkForUnique(key);
 
-// console.log(decoded)
+      let current = obj[key];
+      if (current in dictionary) {
+        obj[key] = dictionary[current];
+      }
+    }
+  }
+
+  for (let i = 0; i < data.length; i += 1) {
+    convert(data[i]);
+  }
+
+  return Object.values(uniqueIds);
+}
+
+console.log(decoded);
